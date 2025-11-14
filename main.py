a57b0346d867254f09f6a3c0c8304900c478988c
dev-karanjaiswar <jaiswarkaran585@gmail.com>
2025-11-14 19:06:26 +0530

    order class added

--- main.py
+++ main.py
@@ -17,19 +17,30 @@ class product:
         return f"{self.item} - ${self.price:.2f} (Qty: {self.quantity})"
     
 class customer:
+    id: int
     name: str
     email: str
     phone: str
     address: str
 
-    def __init__(self, name: str, email: str, phone: str, address: str):
+    def __init__(self,id: int, name: str, email: str, phone: str, address: str):
+        self.id = id
         self.name = name
         self.email = email
         self.phone = phone
         self.address = address
 
-    def contact_info(self):
+    def customer_info(self):
         return f"Name: {self.name}, Email: {self.email}, Phone: {self.phone}, Address: {self.address}"
     
     def __str__(self):
         return f"{self.name} - {self.email}"
+
+
+class order:
+    def __init__(self,c_id: int, pid: str, quantity: int, order_date: str, status: str):
+        self.cust_id = c_id
+        self.prod_id = pid
+        self.quantity = quantity
+        self.order_date = order_date
+        self.status = status
